de7a650c41f7cbfd20024615e238c9de4ec6d177
Otto Cedeno <otto@ottostacos.com>
2019-11-09 00:39:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,6 @@ def reverse_each_word(sentence)
   words_reverse = words.collect {|word| word.reverse}
   words_reverse.join(" ")
 end
+
+# FI answer.  Nice to have on one line!
+# sentence.split.collect {|words| word.reverse}.join(" ")
